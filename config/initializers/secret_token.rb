52957229f17f2856964e51a6fa3882924f8a6b03
Tehras <tkoshkin@gmail.com>
2014-02-15 09:17:37 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Valentines::Application.config.secret_key_base = '0a14fcceba2f37069ab137d689b7a3dafd4f2ac04b6454f2168708fc496d8145cd84fd8fb9b08b55daa21a3d31909642faba750016f31bfe18ce89591b01e301'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Valentines::Application.config.session_store :cookie_store, key: '_valentines_session'
